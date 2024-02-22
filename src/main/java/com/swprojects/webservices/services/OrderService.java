2b2f282da8c2a3eaffb87eda14a4a95307671749
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-02-22 19:35:25 -0300

    Order, Instant, ISO 8601 - PARTE 2

---
+++ src/main/java/com/swprojects/webservices/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.swprojects.webservices.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.swprojects.webservices.entities.Order;
+import com.swprojects.webservices.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	public List<Order> findAll(){
+		return orderRepository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = orderRepository.findById(id);
+		return obj.get();
+	}
+}
