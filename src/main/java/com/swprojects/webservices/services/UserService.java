b8f4dc123a10d78d0517d7932d292516304de185
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-02-22 02:05:23 -0300

    Service layer, component registration

---
+++ src/main/java/com/swprojects/webservices/services/UserService.java
@@ -0,0 +1,26 @@
+package com.swprojects.webservices.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.swprojects.webservices.entities.User;
+import com.swprojects.webservices.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.get();
+	}
+}
