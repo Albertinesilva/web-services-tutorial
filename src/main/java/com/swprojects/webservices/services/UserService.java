119f9ab2c4770429891898ae5703027169c08f68
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-02-23 02:46:50 -0300

    User insert

--- src/main/java/com/swprojects/webservices/services/UserService.java
+++ src/main/java/com/swprojects/webservices/services/UserService.java
@@ -11,16 +11,20 @@ import com.swprojects.webservices.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.get();
+	}
+
+	public User insert(User obj) {
+		return userRepository.save(obj);
 	}
 }
