4f53dc140f4a17c627a84414d3d0888ed7df39c8
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-02-22 22:42:59 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/swprojects/webservices/entities/Category.java
+++ src/main/java/com/swprojects/webservices/entities/Category.java
@@ -1,14 +1,21 @@
 package com.swprojects.webservices.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 
@@ -16,6 +23,13 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+
+	// Utilizada para a JPA não mapear a entidade!
+	// @Transient
+	
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 
 	public Category() {
 	}
@@ -40,6 +54,10 @@ public class Category implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
